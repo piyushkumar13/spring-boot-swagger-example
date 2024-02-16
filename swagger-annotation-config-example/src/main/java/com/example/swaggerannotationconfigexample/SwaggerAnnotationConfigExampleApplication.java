b85018b228ae31880b4cc795e258ca6ee2d82538
piyushkumar13 <piyush.k9013@gmail.com>
2024-02-16 23:51:43 +0530

    initial commit

---
+++ swagger-annotation-config-example/src/main/java/com/example/swaggerannotationconfigexample/SwaggerAnnotationConfigExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.swaggerannotationconfigexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerAnnotationConfigExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerAnnotationConfigExampleApplication.class, args);
+    }
+
+}
---
+++ swagger-api-first-example/src/main/java/com/example/swaggerapifirstexample/SwaggerApiFirstExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.swaggerapifirstexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerApiFirstExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerApiFirstExampleApplication.class, args);
+    }
+
+}
